684ccb970b41fdb354c41533943e67a5be613dd5
Hyunsik Kang <kaka@linecorp.com>
2021-12-30 14:22:10 +0900

    Make different versions Spring Boot Examples

---
+++ examples/spring-boot-2.6/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.springframework.boot") version Dependencies.springBootVersion
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+}
+
+apply(plugin = "org.springframework.boot")
+apply(plugin = "kotlin-spring")
+apply(plugin = "kotlin-jpa")
+
+dependencies {
+    // implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:x.y.z")
+    implementation(Modules.springDataStarter)
+
+    implementation(Dependencies.springBootWeb)
+    implementation(Dependencies.springBootJpa)
+    implementation(Dependencies.jacksonKotlinModule)
+    implementation(Dependencies.h2)
+    implementation(platform(Dependencies.springBootBom))
+
+    testImplementation(Dependencies.springBootTest)
+}
