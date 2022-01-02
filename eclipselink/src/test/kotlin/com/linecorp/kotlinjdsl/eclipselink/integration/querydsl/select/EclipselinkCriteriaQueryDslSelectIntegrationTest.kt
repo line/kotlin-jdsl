d96d23e6e72c5a43668ce24bab9db9f7064f1107
Hyunsik Kang <kaka@linecorp.com>
2022-01-02 15:48:40 +0900

    Add a test for the part where Eclipselink does not explicitly support subquery in select

--- eclipselink/src/test/kotlin/com/linecorp/kotlinjdsl/eclipselink/integration/querydsl/select/EclipselinkCriteriaQueryDslSelectIntegrationTest.kt
+++ eclipselink/src/test/kotlin/com/linecorp/kotlinjdsl/eclipselink/integration/querydsl/select/EclipselinkCriteriaQueryDslSelectIntegrationTest.kt
@@ -2,6 +2,8 @@ package com.linecorp.kotlinjdsl.eclipselink.integration.querydsl.select
 
 import com.linecorp.kotlinjdsl.test.integration.EntityManagerExtension
 import com.linecorp.kotlinjdsl.test.integration.querydsl.select.AbstractCriteriaQueryDslSelectIntegrationTest
+import org.eclipse.persistence.internal.jpa.querydef.SelectionImpl
+import org.eclipse.persistence.internal.jpa.querydef.SubQueryImpl
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import javax.persistence.EntityManager
@@ -10,10 +12,19 @@ import javax.persistence.EntityManager
 class EclipselinkCriteriaQueryDslSelectIntegrationTest : AbstractCriteriaQueryDslSelectIntegrationTest() {
     override lateinit var entityManager: EntityManager
 
+    /**
+     * https://wiki.eclipse.org/EclipseLink/UserGuide/JPA/Basic_JPA_Development/Querying/JPQL#Sub-selects_in_FROM_clause
+     * eclipselink does not support subquery in select and jpa criteria does not support from subquery
+     * so kotlin-jdsl's eclipselink does not support subquery in select
+     */
     @Test
     override fun `listQuery - subquery in select, subquery in from`() {
-        // https://wiki.eclipse.org/EclipseLink/UserGuide/JPA/Basic_JPA_Development/Querying/JPQL#Sub-selects_in_FROM_clause
-        // eclipselink does not support subquery in select and jpa criteria does not support from subquery
-        // so kotlin-jdsl's eclipselink does not support subquery in select
+        // when
+        val exception =
+            catchThrowable(ClassCastException::class) { super.`listQuery - subquery in select, subquery in from`() }
+
+        // then
+        assertThat(exception)
+            .hasMessage("${SubQueryImpl::class.qualifiedName} cannot be cast to ${SelectionImpl::class.qualifiedName}")
     }
 }
