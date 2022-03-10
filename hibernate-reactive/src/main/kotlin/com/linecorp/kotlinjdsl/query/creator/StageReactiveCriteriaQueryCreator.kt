2d10f4b820e0028b57c1cbfc8293cfaed52b8187
Hyunsik Kang <kaka@linecorp.com>
2022-03-10 11:10:43 +0900

    extract JpaCriteriaQueryBuilder interface to object

--- hibernate-reactive/src/main/kotlin/com/linecorp/kotlinjdsl/query/creator/StageReactiveCriteriaQueryCreator.kt
+++ hibernate-reactive/src/main/kotlin/com/linecorp/kotlinjdsl/query/creator/StageReactiveCriteriaQueryCreator.kt
@@ -8,17 +8,17 @@ class StageReactiveCriteriaQueryCreator(
     private val criteriaBuilder: CriteriaBuilder, private val session: Stage.Session
 ) : ReactiveCriteriaQueryCreator {
     override fun <T> createQuery(spec: CriteriaQuerySpec<T, ReactiveQuery<T>>): ReactiveQuery<T> =
-        createQuery(spec = spec, criteriaBuilder = criteriaBuilder) {
+        JpaCriteriaQueryBuilder.createQuery(spec = spec, criteriaBuilder = criteriaBuilder) {
             HibernateStageReactiveQuery(session.createQuery(it))
         }
 
     override fun <T> createQuery(spec: CriteriaUpdateQuerySpec<T, ReactiveQuery<T>>) =
-        createQuery(spec = spec, criteriaBuilder = criteriaBuilder) {
+        JpaCriteriaQueryBuilder.createQuery(spec = spec, criteriaBuilder = criteriaBuilder) {
             HibernateStageReactiveQuery(session.createQuery(it))
         }
 
     override fun <T> createQuery(spec: CriteriaDeleteQuerySpec<T, ReactiveQuery<T>>) =
-        createQuery(spec = spec, criteriaBuilder = criteriaBuilder) {
+        JpaCriteriaQueryBuilder.createQuery(spec = spec, criteriaBuilder = criteriaBuilder) {
             HibernateStageReactiveQuery(session.createQuery(it))
         }
 }
