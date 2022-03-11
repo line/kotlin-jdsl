ed78e329ab7cd2a868017b796f0d74eb99c9902d
Hyunsik Kang <kaka@linecorp.com>
2022-03-11 11:29:06 +0900

    apply runBlocking default Dispatchers.IO

--- test-fixture/integration-reactive/src/main/kotlin/com/linecorp/kotlinjdsl/test/reactive/CoroutineTestExtensions.kt
+++ test-fixture/integration-reactive/src/main/kotlin/com/linecorp/kotlinjdsl/test/reactive/CoroutineTestExtensions.kt
@@ -1,9 +1,9 @@
 package com.linecorp.kotlinjdsl.test.reactive
 
+import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
-fun runBlocking(context: CoroutineContext = EmptyCoroutineContext, block: suspend () -> Unit) {
+fun runBlocking(context: CoroutineContext = Dispatchers.IO, block: suspend () -> Unit) {
     kotlinx.coroutines.runBlocking(context) {
         block()
     }
