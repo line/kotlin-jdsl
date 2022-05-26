baee2ae9e3bd9115f9545496c00684b5da86d61d
Hyunsik Kang <kaka@linecorp.com>
2022-05-27 07:04:04 +0900

    build: upgrade dependencies - spring(boot, data-jpa, core, batch-infra-structure), mockk, h2, mutiny, vertx-jdbc-client, agroal-pool, hibernate, hibernate-reactive

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,9 +4,9 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object Dependencies {
     const val kotlinVersion = "1.6.21"
-    const val springCoreVersion = "5.3.19"
-    const val springBootVersion = "2.6.7"
-    const val springDataJpaVersion = "2.6.4"
+    const val springCoreVersion = "5.3.20"
+    const val springBootVersion = "2.7.0"
+    const val springDataJpaVersion = "2.7.0"
     const val coroutineVersion = "1.6.1"
 
     // kotlin
@@ -17,12 +17,12 @@ object Dependencies {
     const val javaPersistenceApi = "javax.persistence:javax.persistence-api:2.2"
     const val slf4j = "org.slf4j:slf4j-api:1.7.36"
     const val logback = "ch.qos.logback:logback-classic:1.2.11"
-    const val hibernate = "org.hibernate:hibernate-core:5.6.8.Final"
-    const val hibernateReactive = "org.hibernate.reactive:hibernate-reactive-core:1.1.4.Final"
+    const val hibernate = "org.hibernate:hibernate-core:5.6.9.Final"
+    const val hibernateReactive = "org.hibernate.reactive:hibernate-reactive-core:1.1.6.Final"
     const val eclipselink = "org.eclipse.persistence:org.eclipse.persistence.jpa:2.7.10"
     const val jacksonKotlinModule = "com.fasterxml.jackson.module:jackson-module-kotlin"
-    const val agroalPool = "io.agroal:agroal-pool:1.14"
-    const val vertxJdbcClient = "io.vertx:vertx-jdbc-client:4.2.7"
+    const val agroalPool = "io.agroal:agroal-pool:2.0"
+    const val vertxJdbcClient = "io.vertx:vertx-jdbc-client:4.3.1"
     const val coroutineJdk8 = "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutineVersion"
     const val coroutineReactor = "org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutineVersion"
 
@@ -42,16 +42,16 @@ object Dependencies {
     const val springCore = "org.springframework:spring-core:${springCoreVersion}"
     const val springBeans = "org.springframework:spring-beans:${springCoreVersion}"
     const val springJpa = "org.springframework.data:spring-data-jpa:${springDataJpaVersion}"
-    const val springBatchInfrastructure = "org.springframework.batch:spring-batch-infrastructure:4.3.5"
+    const val springBatchInfrastructure = "org.springframework.batch:spring-batch-infrastructure:4.3.6"
 
     // Test
     const val junit = "org.junit.jupiter:junit-jupiter:5.8.2"
     const val assertJ = "org.assertj:assertj-core:3.22.0"
-    const val mockk = "io.mockk:mockk:1.12.3"
+    const val mockk = "io.mockk:mockk:1.12.4"
 
-    const val h2 = "com.h2database:h2:1.4.200"
+    const val h2 = "com.h2database:h2:2.1.212"
 
-    const val mutinyVersion = "1.4.0"
+    const val mutinyVersion = "1.5.0"
     const val mutinyCore = "io.smallrye.reactive:mutiny:$mutinyVersion"
     const val mutinyKotlin = "io.smallrye.reactive:mutiny-kotlin:$mutinyVersion"
     val mutiny = listOf(mutinyCore, mutinyKotlin)
