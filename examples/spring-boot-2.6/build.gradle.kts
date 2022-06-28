46396a2f3b9dfbcd59c41daab81908839e902693
Hyunsik Kang <kaka@linecorp.com>
2022-06-28 12:19:06 +0900

    chore(deps): coroutine 1.6.3, h2 2.1.214, mutiny 1.6.0, spring core 5.3.21, boot 2.7.1(2.6.9) up

--- examples/spring-boot-2.6/build.gradle.kts
+++ examples/spring-boot-2.6/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "2.6.8"
+    id("org.springframework.boot") version "2.6.9"
     kotlin("plugin.spring")
     kotlin("plugin.jpa")
 }
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation(Dependencies.jacksonKotlinModule)
     implementation(Dependencies.h2)
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.6.8"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.6.9"))
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
