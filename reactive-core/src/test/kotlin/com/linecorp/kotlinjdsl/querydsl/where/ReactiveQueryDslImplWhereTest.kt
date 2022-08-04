8b991e06b9b890e66c95cd777e41e2c8eb8017da
jay <jaeykweon@naver.com>
2022-08-05 01:01:46 +0900

    test: add testCase for where (reactive)

--- reactive-core/src/test/kotlin/com/linecorp/kotlinjdsl/querydsl/where/ReactiveQueryDslImplWhereTest.kt
+++ reactive-core/src/test/kotlin/com/linecorp/kotlinjdsl/querydsl/where/ReactiveQueryDslImplWhereTest.kt
@@ -2,6 +2,7 @@ package com.linecorp.kotlinjdsl.querydsl.where
 
 import com.linecorp.kotlinjdsl.query.clause.where.WhereClause
 import com.linecorp.kotlinjdsl.query.spec.predicate.AndSpec
+import com.linecorp.kotlinjdsl.query.spec.predicate.OrSpec
 import com.linecorp.kotlinjdsl.query.spec.predicate.PredicateSpec
 import com.linecorp.kotlinjdsl.querydsl.ReactiveQueryDslImpl
 import com.linecorp.kotlinjdsl.test.WithKotlinJdslAssertions
@@ -15,6 +16,32 @@ internal class ReactiveQueryDslImplWhereTest : WithKotlinJdslAssertions {
         val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
             select(distinct = true, Data1::class.java)
             from(entity(Data1::class))
+        }
+
+        // then
+        val criteriaQuerySpec = actual.createCriteriaQuerySpec()
+
+        assertThat(criteriaQuerySpec.where).isEqualTo(
+            WhereClause(PredicateSpec.empty)
+        )
+
+        val subquerySpec = actual.createSubquerySpec()
+
+        assertThat(subquerySpec.where).isEqualTo(
+            WhereClause(PredicateSpec.empty)
+        )
+    }
+
+    @Test
+    fun nullInWhere() {
+        // given
+        val predicateSpec: PredicateSpec? = null
+
+        // when
+        val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
+            select(distinct = true, Data1::class.java)
+            from(entity(Data1::class))
+            where(predicateSpec)
         }
 
         // then
@@ -58,8 +85,121 @@ internal class ReactiveQueryDslImplWhereTest : WithKotlinJdslAssertions {
     }
 
     @Test
+    fun whereAndVararg() {
+        // given
+        val nullPredicateSpec: PredicateSpec? = null
+        val predicateSpec1: PredicateSpec = mockk()
+        val predicateSpec2: PredicateSpec = mockk()
+
+        // when
+        val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
+            select(distinct = true, Data1::class.java)
+            from(entity(Data1::class))
+            whereAnd(nullPredicateSpec, predicateSpec1, predicateSpec2)
+        }
+
+        // then
+        val criteriaQuerySpec = actual.createCriteriaQuerySpec()
+
+        assertThat(criteriaQuerySpec.where).isEqualTo(
+            WhereClause(AndSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+
+        val subquerySpec = actual.createSubquerySpec()
+
+        assertThat(subquerySpec.where).isEqualTo(
+            WhereClause(AndSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+    }
+
+    @Test
+    fun whereAndList() {
+        // given
+        val nullPredicateSpec: PredicateSpec? = null
+        val predicateSpec1: PredicateSpec = mockk()
+        val predicateSpec2: PredicateSpec = mockk()
+
+        // when
+        val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
+            select(distinct = true, Data1::class.java)
+            from(entity(Data1::class))
+            whereAnd(listOf(nullPredicateSpec, predicateSpec1, predicateSpec2))
+        }
+
+        // then
+        val criteriaQuerySpec = actual.createCriteriaQuerySpec()
+
+        assertThat(criteriaQuerySpec.where).isEqualTo(
+            WhereClause(AndSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+
+        val subquerySpec = actual.createSubquerySpec()
+
+        assertThat(subquerySpec.where).isEqualTo(
+            WhereClause(AndSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+    }
+
+    @Test
+    fun whereOrVararg() {
+        // given
+        val nullPredicateSpec: PredicateSpec? = null
+        val predicateSpec1: PredicateSpec = mockk()
+        val predicateSpec2: PredicateSpec = mockk()
+
+        // when
+        val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
+            select(distinct = true, Data1::class.java)
+            from(entity(Data1::class))
+            whereOr(nullPredicateSpec, predicateSpec1, predicateSpec2)
+        }
+
+        // then
+        val criteriaQuerySpec = actual.createCriteriaQuerySpec()
+
+        assertThat(criteriaQuerySpec.where).isEqualTo(
+            WhereClause(OrSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+
+        val subquerySpec = actual.createSubquerySpec()
+
+        assertThat(subquerySpec.where).isEqualTo(
+            WhereClause(OrSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+    }
+
+    @Test
+    fun whereOrList() {
+        // given
+        val nullPredicateSpec: PredicateSpec? = null
+        val predicateSpec1: PredicateSpec = mockk()
+        val predicateSpec2: PredicateSpec = mockk()
+
+        // when
+        val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
+            select(distinct = true, Data1::class.java)
+            from(entity(Data1::class))
+            whereOr(listOf(nullPredicateSpec, predicateSpec1, predicateSpec2))
+        }
+
+        // then
+        val criteriaQuerySpec = actual.createCriteriaQuerySpec()
+
+        assertThat(criteriaQuerySpec.where).isEqualTo(
+            WhereClause(OrSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+
+        val subquerySpec = actual.createSubquerySpec()
+
+        assertThat(subquerySpec.where).isEqualTo(
+            WhereClause(OrSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+    }
+
+    @Test
     fun wheres() {
         // given
+        val nullPredicateSpec: PredicateSpec? = null
         val predicateSpec1: PredicateSpec = mockk()
         val predicateSpec2: PredicateSpec = mockk()
 
@@ -82,6 +222,53 @@ internal class ReactiveQueryDslImplWhereTest : WithKotlinJdslAssertions {
 
         assertThat(subquerySpec.where).isEqualTo(
             WhereClause(AndSpec(listOf(predicateSpec1, predicateSpec2)))
+        )
+    }
+
+    @Test
+    fun allTypeWheres() {
+        // given
+        val nullPredicateSpec: PredicateSpec? = null
+        val predicateSpec1: PredicateSpec = mockk()
+        val predicateSpec2: PredicateSpec = mockk()
+
+        // when
+        val actual = ReactiveQueryDslImpl(Data1::class.java).apply {
+            select(distinct = true, Data1::class.java)
+            from(entity(Data1::class))
+            where(nullPredicateSpec)
+            where(predicateSpec1)
+            whereAnd(nullPredicateSpec, predicateSpec1, predicateSpec2)
+            whereOr(nullPredicateSpec, predicateSpec1, predicateSpec2)
+        }
+
+        // then
+        val criteriaQuerySpec = actual.createCriteriaQuerySpec()
+
+        assertThat(criteriaQuerySpec.where).isEqualTo(
+            WhereClause(
+                AndSpec(
+                    listOf(
+                        predicateSpec1,
+                        AndSpec(listOf(predicateSpec1, predicateSpec2)),
+                        OrSpec(listOf(predicateSpec1, predicateSpec2))
+                    )
+                )
+            )
+        )
+
+        val subquerySpec = actual.createSubquerySpec()
+
+        assertThat(subquerySpec.where).isEqualTo(
+            WhereClause(
+                AndSpec(
+                    listOf(
+                        predicateSpec1,
+                        AndSpec(listOf(predicateSpec1, predicateSpec2)),
+                        OrSpec(listOf(predicateSpec1, predicateSpec2))
+                    )
+                )
+            )
         )
     }
 
