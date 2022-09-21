7f2f2d559b42a84cabace68f411a86fb8c2acce7
Hyunsik Kang <hyunsik.kang@superlabs.us>
2022-09-22 06:56:56 +0900

    chore: typo fix (mitiny -> mutiny), mutiny version 1.7.0

--- spring/data-hibernate-reactive/build.gradle.kts
+++ spring/data-hibernate-reactive/build.gradle.kts
@@ -3,7 +3,7 @@ apply<PublishPlugin>()
 dependencies {
     compileOnly(libs.spring.jpa)
     compileOnly(libs.hibernate.reactive)
-    compileOnly(libs.bundles.mitiny)
+    compileOnly(libs.bundles.mutiny)
 
     api(Modules.reactiveCore)
     api(Modules.hibernateReactive)
@@ -14,7 +14,7 @@ dependencies {
     testImplementation(Modules.testFixtureEntity)
     testImplementation(Modules.testFixtureHibernateReactive)
     testImplementation(Modules.testFixtureIntegrationReactive)
-    testImplementation(libs.bundles.mitiny)
+    testImplementation(libs.bundles.mutiny)
     testImplementation(libs.spring.boot.test)
     testImplementation(libs.spring.jpa)
     testImplementation(libs.hibernate.reactive)
