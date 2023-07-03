37a3e9028404697816640041a40f7cac53242d37
jonghyon.s <jonghyon.s@linecorp.com>
2023-07-03 16:21:52 +0900

    docs(render): remove invalid comment in sql serializer

--- render/sql/src/main/kotlin/com/linecorp/kotlinjdsl/render/sql/serializer/SqlSerializer.kt
+++ render/sql/src/main/kotlin/com/linecorp/kotlinjdsl/render/sql/serializer/SqlSerializer.kt
@@ -8,8 +8,5 @@ import kotlin.reflect.KClass
 interface SqlSerializer<T : QueryPart> {
     fun handledType(): KClass<T>
 
-    /**
-     * QuerySerializer의 결과가 되는 String은 앞 뒤로 공백이 없는 것을 기본으로 한다.
-     */
     fun serialize(part: T, writer: SqlWriter, context: RenderContext)
 }
