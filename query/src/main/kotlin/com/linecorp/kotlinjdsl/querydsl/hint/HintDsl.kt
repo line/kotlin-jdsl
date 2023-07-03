6d29b8d1d09f2831be31b5ea7b2eb1f36c58c98c
jonghyon.s <jonghyon.s@linecorp.com>
2023-07-03 10:45:39 +0900

    refactor(query): remove old query code

--- query/src/main/kotlin/com/linecorp/kotlinjdsl/querydsl/hint/HintDsl.kt
+++
@@ -1,9 +0,0 @@
-package com.linecorp.kotlinjdsl.querydsl.hint
-
-interface HintDsl {
-    fun sqlHints(vararg hints: String) = sqlHints(hints.toList())
-    fun sqlHints(hints: List<String>)
-
-    fun hints(vararg hints: Pair<String, Any>) = hints(hints.toMap())
-    fun hints(hints: Map<String, Any>)
-}
