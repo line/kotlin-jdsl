0dcb5460b976796b6566a1bea411987821e2723c
Hyunsik Kang <cj848@hanmail.net>
2023-07-17 09:24:36 +0900

    fix: compile error

--- query-model/sql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/sql/impl/NormalDeleteQuery.kt
+++ query-model/sql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/sql/impl/NormalDeleteQuery.kt
@@ -1,6 +1,7 @@
 package com.linecorp.kotlinjdsl.querymodel.sql.impl
 
 import com.linecorp.kotlinjdsl.Internal
+import com.linecorp.kotlinjdsl.querymodel.sql.DeleteQuery
 import com.linecorp.kotlinjdsl.querymodel.sql.Expression
 import com.linecorp.kotlinjdsl.querymodel.sql.Predicate
 import com.linecorp.kotlinjdsl.querymodel.sql.TableReference
@@ -10,4 +11,4 @@ data class NormalDeleteQuery<T : Any>(
     val table: TableReference<T>,
     val where: Predicate?,
     val orderBy: Collection<Expression<*>>?,
-) : _root_ide_package_.com.linecorp.kotlinjdsl.querymodel.sql.DeleteQuery<T>
+) : DeleteQuery<T>
