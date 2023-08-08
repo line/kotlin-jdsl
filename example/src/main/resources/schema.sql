c1b9efbfd404b542fd2682651c20145efec921ae
jonghyon.s <jonghyon.s@linecorp.com>
2023-08-08 10:28:39 +0900

    test(example): example schema

--- example/src/main/resources/schema.sql
+++ example/src/main/resources/schema.sql
@@ -1,27 +1,27 @@
 create table author
 (
-    author_id        identity     not null primary key,
+    author_id        bigint auto_increment not null primary key,
 
-    name             varchar(255) not null
+    name             varchar(255)          not null
 );
 
 create table publisher
 (
-    publisher_id identity     not null primary key,
+    publisher_id bigint auto_increment not null primary key,
 
-    name         varchar(255) not null
+    name         varchar(255)          not null
 );
 
 create table book
 (
-    isbn         varchar(13)              not null primary key,
+    isbn         varchar(13)    not null primary key,
 
-    title        varchar(255)             not null,
-    image_url    varchar(255)             not null,
-    publish_date timestamp with time zone not null,
+    title        varchar(255)   not null,
+    image_url    varchar(255)   not null,
+    publish_date datetime(6)    not null,
 
-    price        numeric(38, 2)           not null,
-    sale_price   numeric(38, 2)           not null
+    price        numeric(38, 2) not null,
+    sale_price   numeric(38, 2) not null
 );
 
 create table book_author
@@ -40,25 +40,25 @@ create table book_publisher
 
 create table department
 (
-    department_id identity     not null primary key,
+    department_id bigint auto_increment not null primary key,
 
-    name          varchar(255) not null
+    name          varchar(255)          not null
 );
 
 create table employee
 (
-    employee_id      identity     not null primary key,
-    employee_type    varchar(31)  not null,
+    employee_id      bigint auto_increment not null primary key,
+    employee_type    varchar(31)           not null,
 
-    name             varchar(255) not null,
+    name             varchar(255)          not null,
     nickname         varchar(255),
-    phone            varchar(255) not null,
+    phone            varchar(255)          not null,
 
-    zip_code         varchar(255) not null,
-    street_address_1 varchar(255) not null,
+    zip_code         varchar(255)          not null,
+    street_address_1 varchar(255)          not null,
     street_address_2 varchar(255),
-    city             varchar(255) not null,
-    province         varchar(255) not null,
+    city             varchar(255)          not null,
+    province         varchar(255)          not null,
 
     annual_salary    numeric(38, 2),
     weekly_salary    numeric(38, 2)
