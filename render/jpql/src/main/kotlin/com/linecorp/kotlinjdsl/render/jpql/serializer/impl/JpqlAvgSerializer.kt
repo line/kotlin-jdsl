b1eb49a50becd51fc885505e537773d6ac43f9e5
Jake Son <sonjeabin@gmail.com>
2023-08-08 22:53:00 +0900

    feat(render): add sum and avg jpql serializers

---
+++ render/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/render/jpql/serializer/impl/JpqlAvgSerializer.kt
@@ -0,0 +1,30 @@
+package com.linecorp.kotlinjdsl.render.jpql.serializer.impl
+
+import com.linecorp.kotlinjdsl.querymodel.jpql.expression.impl.JpqlAvg
+import com.linecorp.kotlinjdsl.render.RenderContext
+import com.linecorp.kotlinjdsl.render.jpql.serializer.JpqlRenderSerializer
+import com.linecorp.kotlinjdsl.render.jpql.serializer.JpqlSerializer
+import com.linecorp.kotlinjdsl.render.jpql.writer.JpqlWriter
+import kotlin.reflect.KClass
+
+class JpqlAvgSerializer : JpqlSerializer<JpqlAvg<*>> {
+    override fun handledType(): KClass<JpqlAvg<*>> {
+        return JpqlAvg::class
+    }
+
+    override fun serialize(part: JpqlAvg<*>, writer: JpqlWriter, context: RenderContext) {
+        val delegate = context.getValue(JpqlRenderSerializer)
+
+        writer.write("AVG")
+        writer.write("(")
+
+        if (part.distinct) {
+            writer.write("DISTINCT")
+            writer.write(" ")
+        }
+
+        delegate.serialize(part.expr, writer, context)
+
+        writer.write(")")
+    }
+}
