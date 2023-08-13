6bd3ae505e3cb85f34182268b797d1a886f4d273
Jake Son <sonjeabin@gmail.com>
2023-08-14 07:06:04 +0900

    test(render): add JpqlInSubquerySerializer test

--- render/jpql/src/test/kotlin/com/linecorp/kotlinjdsl/render/jpql/serializer/impl/JpqlInSubquerySerializerTest.kt
+++ render/jpql/src/test/kotlin/com/linecorp/kotlinjdsl/render/jpql/serializer/impl/JpqlInSubquerySerializerTest.kt
@@ -1,25 +1,21 @@
 package com.linecorp.kotlinjdsl.render.jpql.serializer.impl
 
-import com.linecorp.kotlinjdsl.querymodel.jpql.entity.Entities
 import com.linecorp.kotlinjdsl.querymodel.jpql.expression.Expressions
-import com.linecorp.kotlinjdsl.querymodel.jpql.join.Joins
-import com.linecorp.kotlinjdsl.querymodel.jpql.join.impl.JpqlInnerJoin
 import com.linecorp.kotlinjdsl.querymodel.jpql.predicate.Predicates
+import com.linecorp.kotlinjdsl.querymodel.jpql.predicate.impl.JpqlInSubquery
+import com.linecorp.kotlinjdsl.querymodel.jpql.select.Selects
 import com.linecorp.kotlinjdsl.render.TestRenderContext
 import com.linecorp.kotlinjdsl.render.jpql.serializer.JpqlRenderSerializer
 import com.linecorp.kotlinjdsl.render.jpql.serializer.JpqlSerializerTest
 import com.linecorp.kotlinjdsl.render.jpql.writer.JpqlWriter
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.just
-import io.mockk.runs
-import io.mockk.verifySequence
 import org.assertj.core.api.WithAssertions
 import org.junit.jupiter.api.Test
 
 @JpqlSerializerTest
-class JpqlInnerJoinSerializerTest : WithAssertions {
-    private val sut = JpqlInnerJoinSerializer()
+class JpqlInSubquerySerializerTest : WithAssertions {
+    private val sut = JpqlInSubquerySerializer()
     data class TestEntity(val id: Long, val name: String)
 
     @MockK
@@ -34,39 +30,40 @@ class JpqlInnerJoinSerializerTest : WithAssertions {
         val actual = sut.handledType()
 
         // then
-        assertThat(actual).isEqualTo(JpqlInnerJoin::class)
+        assertThat(actual).isEqualTo(JpqlInSubquery::class)
     }
 
     @Test
-    fun `serialize - WHEN inner join is given, THEN draw full syntax`() {
+    fun `serialize - WHEN in subquery is given, THEN draw full syntax`() {
         // given
         every { writer.write(any<String>()) } just runs
         every { serializer.serialize(any(), any(), any()) } just runs
 
-        val part = Joins.innerJoin(
-            Entities.entity(TestEntity::class, "test"),
-            Predicates.equal(
-                Expressions.stringLiteral("value"),
-                Expressions.stringLiteral("compare"),
-            ),
+        val select = Selects.select(
+            returnType = TestEntity::class,
+            distinct = false,
+            select = emptyList(),
+            from = emptyList(),
+            where = null,
+            groupBy = null,
+            having = null,
+            orderBy = null,
         )
+        val part = Predicates.`in`(Expressions.expression(TestEntity::class, "test"), Expressions.subquery(select))
         val context = TestRenderContext(serializer)
 
         // when
-        sut.serialize(part as JpqlInnerJoin<*>, writer, context)
+        sut.serialize(part as JpqlInSubquery<*>, writer, context)
 
         // then
         verifySequence {
-            writer.write("INNER JOIN")
-            writer.write(" ")
-
-            serializer.serialize(part.entity, writer, context)
+            serializer.serialize(part.value, writer, context)
 
             writer.write(" ")
-            writer.write("ON")
+            writer.write("IN")
             writer.write(" ")
 
-            serializer.serialize(part.on, writer, context)
+            serializer.serialize(part.subquery, writer, context)
         }
     }
 }
