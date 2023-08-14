18cf587af15faf9b97196bf106ce4d245fac86e7
Jake Son <sonjeabin@gmail.com>
2023-08-14 21:29:02 +0900

    refactor: make JpqlJoinedEntity internal

--- query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/from/impl/JpqlJoinedEntity.kt
+++ query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/from/impl/JpqlJoinedEntity.kt
@@ -1,11 +1,13 @@
 package com.linecorp.kotlinjdsl.querymodel.jpql.from.impl
 
+import com.linecorp.kotlinjdsl.Internal
 import com.linecorp.kotlinjdsl.querymodel.jpql.entity.Entity
 import com.linecorp.kotlinjdsl.querymodel.jpql.from.From
 import com.linecorp.kotlinjdsl.querymodel.jpql.join.Join
 import com.linecorp.kotlinjdsl.querymodel.jpql.join.JoinType
 
-data class JpqlJoinedEntity(
+@Internal
+data class JpqlJoinedEntity internal constructor(
     val entity: Entity<*>,
     val join: Join,
 ) : Entity<Any>, Join {
