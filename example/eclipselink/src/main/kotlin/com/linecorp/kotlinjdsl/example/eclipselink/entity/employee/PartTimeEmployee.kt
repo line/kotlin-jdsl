25fa6b57be82762ab4651965c0f56ee7e2f9f98b
SeongYunKim <cauboy17@naver.com>
2023-08-31 16:41:56 +0900

    chore: remove unused import

--- example/eclipselink/src/main/kotlin/com/linecorp/kotlinjdsl/example/eclipselink/entity/employee/PartTimeEmployee.kt
+++ example/eclipselink/src/main/kotlin/com/linecorp/kotlinjdsl/example/eclipselink/entity/employee/PartTimeEmployee.kt
@@ -1,9 +1,5 @@
 package com.linecorp.kotlinjdsl.example.eclipselink.entity.employee
 
-import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.Employee
-import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.EmployeeAddress
-import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.EmployeeDepartment
-import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.EmployeeSalary
 import jakarta.persistence.*
 
 @Entity
