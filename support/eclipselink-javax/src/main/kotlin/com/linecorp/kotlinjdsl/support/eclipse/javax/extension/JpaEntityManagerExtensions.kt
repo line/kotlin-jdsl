b8935e019ad3919a6c6f4e6d6018fd74ce8fdb05
SeongYunKim <cauboy17@naver.com>
2023-09-04 18:19:52 +0900

    chore: add comment on JpaEntityManagerExtensions

--- support/eclipselink-javax/src/main/kotlin/com/linecorp/kotlinjdsl/support/eclipse/javax/extension/JpaEntityManagerExtensions.kt
+++ support/eclipselink-javax/src/main/kotlin/com/linecorp/kotlinjdsl/support/eclipse/javax/extension/JpaEntityManagerExtensions.kt
@@ -1,41 +1,68 @@
 package com.linecorp.kotlinjdsl.support.eclipse.javax.extension
 
+import com.linecorp.kotlinjdsl.SinceJdsl
 import com.linecorp.kotlinjdsl.querymodel.jpql.delete.DeleteQuery
 import com.linecorp.kotlinjdsl.querymodel.jpql.select.SelectQuery
 import com.linecorp.kotlinjdsl.querymodel.jpql.update.UpdateQuery
 import com.linecorp.kotlinjdsl.render.RenderContext
 import com.linecorp.kotlinjdsl.support.eclipse.javax.JpqlJpaEntityManagerUtils
 import org.eclipse.persistence.jpa.JpaEntityManager
+import javax.persistence.Query
+import javax.persistence.TypedQuery
 
+/**
+ * Creates [javax.persistence.TypedQuery] from the [SelectQuery] and [RenderContext].
+ */
+@SinceJdsl("3.0.0")
 fun <T : Any> JpaEntityManager.createQuery(
     query: SelectQuery<T>,
     context: RenderContext,
-) = JpqlJpaEntityManagerUtils.createQuery(this, query, context)
+): TypedQuery<T> = JpqlJpaEntityManagerUtils.createQuery(this, query, context)
 
+/**
+ * Creates [javax.persistence.TypedQuery] from the [SelectQuery] and [RenderContext].
+ */
+@SinceJdsl("3.0.0")
 fun <T : Any> JpaEntityManager.createQuery(
     query: SelectQuery<T>,
     queryParams: Map<String, Any?>,
     context: RenderContext,
-) = JpqlJpaEntityManagerUtils.createQuery(this, query, queryParams, context)
+): TypedQuery<T> = JpqlJpaEntityManagerUtils.createQuery(this, query, queryParams, context)
 
+/**
+ * Creates [javax.persistence.Query] from the [UpdateQuery] and [RenderContext].
+ */
+@SinceJdsl("3.0.0")
 fun <T : Any> JpaEntityManager.createQuery(
     query: UpdateQuery<T>,
     context: RenderContext,
-) = JpqlJpaEntityManagerUtils.createQuery(this, query, context)
+): Query = JpqlJpaEntityManagerUtils.createQuery(this, query, context)
 
+/**
+ * Creates [javax.persistence.Query] from the [UpdateQuery] and [RenderContext].
+ */
+@SinceJdsl("3.0.0")
 fun <T : Any> JpaEntityManager.createQuery(
     query: UpdateQuery<T>,
     queryParams: Map<String, Any?>,
     context: RenderContext,
-) = JpqlJpaEntityManagerUtils.createQuery(this, query, queryParams, context)
+): Query = JpqlJpaEntityManagerUtils.createQuery(this, query, queryParams, context)
 
+/**
+ * Creates [javax.persistence.Query] from the [DeleteQuery] and [RenderContext].
+ */
+@SinceJdsl("3.0.0")
 fun <T : Any> JpaEntityManager.createQuery(
     query: DeleteQuery<T>,
     context: RenderContext,
-) = JpqlJpaEntityManagerUtils.createQuery(this, query, context)
+): Query = JpqlJpaEntityManagerUtils.createQuery(this, query, context)
 
+/**
+ * Creates [javax.persistence.Query] from the [DeleteQuery] and [RenderContext].
+ */
+@SinceJdsl("3.0.0")
 fun <T : Any> JpaEntityManager.createQuery(
     query: DeleteQuery<T>,
     queryParams: Map<String, Any?>,
     context: RenderContext,
-) = JpqlJpaEntityManagerUtils.createQuery(this, query, queryParams, context)
+): Query = JpqlJpaEntityManagerUtils.createQuery(this, query, queryParams, context)
