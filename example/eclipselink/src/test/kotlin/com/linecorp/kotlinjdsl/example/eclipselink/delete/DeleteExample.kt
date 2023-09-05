994e13b9e71f174f7d6d80c10a7f86e060c1ac2f
SeongYunKim <cauboy17@naver.com>
2023-09-06 02:46:36 +0900

    feat: implement Update, Delete Example

---
+++ example/eclipselink/src/test/kotlin/com/linecorp/kotlinjdsl/example/eclipselink/delete/DeleteExample.kt
@@ -0,0 +1,173 @@
+package com.linecorp.kotlinjdsl.example.eclipselink.delete
+
+import com.linecorp.kotlinjdsl.dsl.jpql.jpql
+import com.linecorp.kotlinjdsl.example.eclipselink.entity.book.Book
+import com.linecorp.kotlinjdsl.example.eclipselink.entity.book.Isbn
+import com.linecorp.kotlinjdsl.example.eclipselink.entity.department.Department
+import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.Employee
+import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.EmployeeDepartment
+import com.linecorp.kotlinjdsl.render.jpql.JpqlRenderContext
+import com.linecorp.kotlinjdsl.support.eclipselink.extension.createQuery
+import jakarta.persistence.Persistence
+import java.time.OffsetDateTime
+import org.assertj.core.api.WithAssertions
+import org.eclipse.persistence.jpa.JpaEntityManager
+import org.junit.jupiter.api.Test
+
+class DeleteExample : WithAssertions {
+    private val entityManagerFactory = Persistence.createEntityManagerFactory("example")
+
+    private val context = JpqlRenderContext()
+
+    @Test
+    fun `delete all books published after June 2023`() {
+        // given
+        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+        val deleteJpqlQuery = jpql {
+            deleteFrom(
+                entity(Book::class),
+            ).where(
+                path(Book::publishDate).ge(OffsetDateTime.parse("2023-06-01T00:00:00+09:00")),
+            )
+        }
+        val selectJpqlQuery = jpql {
+            select(
+                entity(Book::class),
+            ).from(
+                entity(Book::class),
+            )
+        }
+
+        // when
+        val transaction = entityManger.transaction
+        transaction.begin()
+        entityManger.createQuery(deleteJpqlQuery, context).executeUpdate()
+        transaction.commit()
+        val actual = entityManger.createQuery(selectJpqlQuery, context).resultList
+
+        // then
+        assertThat(actual.map { it.isbn }).isEqualTo(
+            listOf(
+                Isbn("01"),
+                Isbn("02"),
+                Isbn("03"),
+                Isbn("04"),
+                Isbn("05"),
+            ),
+        )
+    }
+
+    @Test
+    fun `delete the employees from department 03`() {
+        // given
+        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+        val deleteJpqlQuery = jpql {
+            val employeeIds = select<Long>(
+                path(EmployeeDepartment::employee)(Employee::employeeId),
+            ).from(
+                entity(Department::class),
+                join(EmployeeDepartment::class)
+                    .on(path(Department::departmentId).equal(path(EmployeeDepartment::departmentId))),
+            ).where(
+                path(Department::name).like("%03"),
+            ).asSubquery()
+
+            deleteFrom(
+                entity(Employee::class),
+            ).where(
+                path(Employee::employeeId).`in`(employeeIds),
+            )
+        }
+        val selectJpqlQuery = jpql {
+            select(
+                entity(Employee::class),
+            ).from(
+                entity(Employee::class),
+            )
+        }
+
+        // when
+        val transaction = entityManger.transaction
+        transaction.begin()
+        entityManger.createQuery(deleteJpqlQuery, context).executeUpdate()
+        transaction.commit()
+        val actual = entityManger.createQuery(selectJpqlQuery, context).resultList
+
+        // then
+        assertThat(actual).isEqualTo(
+            listOf(
+                2L,
+                3L,
+                4L,
+                5L,
+                6L,
+                15L,
+                16L,
+                17L,
+                18L,
+                19L,
+                20L,
+                22L,
+            ),
+        )
+    }
+
+//    @Test
+//    fun `remove the employees from department 03`() {
+//        // when
+//        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+//
+//        val deleteJpqlQuery = jpql {
+//            val employeeIds = select<Long>(
+//                path(EmployeeDepartment::employee)(Employee::employeeId),
+//            ).from(
+//                entity(Department::class),
+//                join(EmployeeDepartment::class)
+//                    .on(path(Department::departmentId).equal(path(EmployeeDepartment::departmentId))),
+//            ).where(
+//                path(Department::name).like("%03"),
+//            ).asSubquery()
+//
+//            deleteFrom(
+//                entity(Employee::class),
+//            ).where(
+//                path(Employee::employeeId).`in`(employeeIds),
+//            )
+//        }
+//        val selectJpqlQuery = jpql {
+//            select(path(Employee::employeeId))
+//                .from(entity(Employee::class))
+//        }
+//
+//        // when
+//        entityManger.transaction.run {
+//            begin()
+//            println(entityManger.createQuery(selectJpqlQuery, context).resultList)
+//            entityManger.createQuery(deleteJpqlQuery, context).executeUpdate()
+//            println(entityManger.createQuery(selectJpqlQuery, context).resultList)
+//            commit()
+//        }
+//        val actual = entityManger.createQuery(selectJpqlQuery, context).resultList
+//
+////        println(entityManger.createQuery(selectJpqlQuery, context).resultList)
+////        println(entityManger.createQuery(selectJpqlQuery, context).resultList)
+//
+//        // then
+//        assertThat(actual).isEqualTo(
+//            listOf(
+//                2L,
+//                3L,
+//                4L,
+//                5L,
+//                6L,
+//                15L,
+//                16L,
+//                17L,
+//                18L,
+//                19L,
+//                20L,
+//                22L,
+//            ),
+//        )
+//    }
+}
--- example/eclipselink/src/test/kotlin/com/linecorp/kotlinjdsl/example/eclipselink/select/SelectExample.kt
+++ example/eclipselink/src/test/kotlin/com/linecorp/kotlinjdsl/example/eclipselink/select/SelectExample.kt
@@ -4,6 +4,7 @@ import com.linecorp.kotlinjdsl.dsl.jpql.jpql
 import com.linecorp.kotlinjdsl.example.eclipselink.entity.author.Author
 import com.linecorp.kotlinjdsl.example.eclipselink.entity.book.Book
 import com.linecorp.kotlinjdsl.example.eclipselink.entity.book.BookAuthor
+import com.linecorp.kotlinjdsl.example.eclipselink.entity.book.BookPrice
 import com.linecorp.kotlinjdsl.example.eclipselink.entity.book.Isbn
 import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.Employee
 import com.linecorp.kotlinjdsl.example.eclipselink.entity.employee.EmployeeDepartment
@@ -13,15 +14,17 @@ import jakarta.persistence.Persistence
 import org.assertj.core.api.WithAssertions
 import org.eclipse.persistence.jpa.JpaEntityManager
 import org.junit.jupiter.api.Test
+import java.time.OffsetDateTime
 
 class SelectExample : WithAssertions {
     private val entityManagerFactory = Persistence.createEntityManagerFactory("example")
+
+    private val context = JpqlRenderContext()
 
     @Test
     fun `the most prolific author`() {
         // given
         val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
         val jpqlQuery = jpql {
             select(
                 path(Author::authorId),
@@ -46,7 +49,6 @@ class SelectExample : WithAssertions {
     fun `authors who haven't written a book`() {
         // given
         val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
         val jpqlQuery = jpql {
             select(
                 path(Author::authorId),
@@ -68,33 +70,9 @@ class SelectExample : WithAssertions {
     }
 
     @Test
-    fun books() {
-        // given
-//        val pageable = PageRequest.of(1, 3, Sort.by(Sort.Direction.ASC, "isbn"))
-
-        // given
-        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
-        val jpqlQuery = jpql {
-            select(
-                path(Book::isbn),
-            ).from(
-                entity(Book::class),
-            )
-        }
-
-        // when
-        val actual = entityManger.createQuery(jpqlQuery, context).resultList
-
-        // then
-        assertThat(actual).isEqualTo(listOf(Isbn("04"), Isbn("05"), Isbn("06")))
-    }
-
-    @Test
     fun `the book with the most authors`() {
         // given
         val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
         val jpqlQuery = jpql {
             select(
                 path(Book::isbn),
@@ -115,101 +93,111 @@ class SelectExample : WithAssertions {
         assertThat(actual).isEqualTo(Isbn("01"))
     }
 
-//    @Test
-//    fun `the most expensive book`() {
-//        // given
-//        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-//        val context = JpqlRenderContext()
-//        val jpqlQuery = jpql {
-//            select(
-//                path(Book::isbn),
-//            ).from(
-//                entity(Book::class),
-//            ).orderBy(
-//                path(Book::salePrice).desc(),
-//                path(Book::isbn).asc(),
-//            )
-//        }
-//
-//        // when
-//        val actual = entityManger.createQuery(jpqlQuery, context).setMaxResults(1).resultList.firstOrNull()
-//
-//        // then
-//        assertThat(actual).isEqualTo(Isbn("10"))
-//    }
+    @Test
+    fun `the most expensive book`() {
+        // given
+        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+        val jpqlQuery = jpql {
+            select(
+                path(Book::isbn),
+            ).from(
+                entity(Book::class),
+            ).orderBy(
+                path(Book::salePrice).desc(),
+                path(Book::isbn).asc(),
+            )
+        }
 
-    //
-//    @Test
-//    fun `the most recently published book`() {
-//        // when
-//        val actual = bookRepository.findFirst {
-//            select(
-//                path(Book::isbn),
-//            ).from(
-//                entity(Book::class),
-//            ).orderBy(
-//                path(Book::salePrice).desc(),
-//                path(Book::isbn).asc(),
-//            )
-//        }
-//
-//        // then
-//        assertThat(actual).isEqualTo(Isbn("10"))
-//    }
-//
-//    @Test
-//    fun `books published between January and June 2023`() {
-//        // when
-//        val actual = bookRepository.findAll {
-//            select(
-//                path(Book::isbn),
-//            ).from(
-//                entity(Book::class),
-//            ).where(
-//                path(Book::publishDate).between(
-//                    OffsetDateTime.parse("2023-01-01T00:00:00+09:00"),
-//                    OffsetDateTime.parse("2023-06-30T23:59:59+09:00"),
-//                ),
-//            ).orderBy(
-//                path(Book::isbn).asc(),
-//            )
-//        }
-//
-//        // then
-//        assertThat(actual).isEqualTo(
-//            listOf(
-//                Isbn("01"),
-//                Isbn("02"),
-//                Isbn("03"),
-//                Isbn("04"),
-//                Isbn("05"),
-//                Isbn("06"),
-//            ),
-//        )
-//    }
-//
-//    @Test
-//    fun `the book with the biggest discounts`() {
-//        // when
-//        val actual = bookRepository.findFirst {
-//            select(
-//                path(Book::isbn),
-//            ).from(
-//                entity(Book::class),
-//            ).orderBy(
-//                path(Book::price)(BookPrice::value).minus(path(Book::salePrice)(BookPrice::value)).desc(),
-//            )
-//        }
-//
-//        // then
-//        assertThat(actual).isEqualTo(Isbn("12"))
-//    }
-//
+        // when
+        val actual = entityManger.createQuery(jpqlQuery, context).setMaxResults(1).singleResult
+
+        // then
+        assertThat(actual).isEqualTo(Isbn("10"))
+    }
+
+
+    @Test
+    fun `the most recently published book`() {
+        // given
+        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+        val jpqlQuery = jpql {
+            select(
+                path(Book::isbn),
+            ).from(
+                entity(Book::class),
+            ).orderBy(
+                path(Book::salePrice).desc(),
+                path(Book::isbn).asc(),
+            )
+        }
+
+        // when
+        val actual = entityManger.createQuery(jpqlQuery, context).setMaxResults(1).singleResult
+
+        // then
+        assertThat(actual).isEqualTo(Isbn("10"))
+    }
+
+    @Test
+    fun `books published between January and June 2023`() {
+        // given
+        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+        val jpqlQuery = jpql {
+            select(
+                path(Book::isbn),
+            ).from(
+                entity(Book::class),
+            ).where(
+                path(Book::publishDate).between(
+                    OffsetDateTime.parse("2023-01-01T00:00:00+09:00"),
+                    OffsetDateTime.parse("2023-06-30T23:59:59+09:00"),
+                ),
+            ).orderBy(
+                path(Book::isbn).asc(),
+            )
+        }
+
+        // when
+        val actual = entityManger.createQuery(jpqlQuery, context).resultList
+
+        // then
+        assertThat(actual).isEqualTo(
+            listOf(
+                Isbn("01"),
+                Isbn("02"),
+                Isbn("03"),
+                Isbn("04"),
+                Isbn("05"),
+                Isbn("06"),
+            ),
+        )
+    }
+
+    @Test
+    fun `the book with the biggest discounts`() {
+        // given
+        val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
+        val jpqlQuery = jpql {
+            select(
+                path(Book::isbn),
+            ).from(
+                entity(Book::class),
+            ).orderBy(
+                path(Book::price)(BookPrice::value).minus(path(Book::salePrice)(BookPrice::value)).desc(),
+            )
+        }
+
+        // when
+        val actual = entityManger.createQuery(jpqlQuery, context).setMaxResults(1).singleResult
+
+        // then
+        assertThat(actual).isEqualTo(Isbn("12"))
+    }
+
     @Test
     fun `employees without a nickname`() {
         // given
         val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
         val jpqlQuery = jpql {
             select(
                 path(Employee::employeeId),
@@ -256,7 +244,6 @@ class SelectExample : WithAssertions {
         )
 
         val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
         val jpqlQuery = jpql {
             selectNew<Row>(
                 path(EmployeeDepartment::departmentId),
@@ -288,25 +275,24 @@ class SelectExample : WithAssertions {
     fun `the number of employees who belong to more than one department`() {
         // given
         val entityManger = entityManagerFactory.createEntityManager().unwrap(JpaEntityManager::class.java)
-        val context = JpqlRenderContext()
         val jpqlQuery = jpql {
+            val subquery = select(
+                path(Employee::employeeId),
+            ).from(
+                entity(Employee::class),
+                join(Employee::departments),
+            ).groupBy(
+                path(Employee::employeeId),
+            ).having(
+                count(Employee::employeeId).greaterThan(1L),
+            ).asSubquery()
+
             select(
                 count(Employee::employeeId),
             ).from(
                 entity(Employee::class),
             ).where(
-                path(Employee::employeeId).`in`(
-                    select(
-                        path(Employee::employeeId),
-                    ).from(
-                        entity(Employee::class),
-                        join(Employee::departments),
-                    ).groupBy(
-                        path(Employee::employeeId),
-                    ).having(
-                        count(Employee::employeeId).greaterThan(1L),
-                    ).asSubquery(),
-                ),
+                path(Employee::employeeId).`in`(subquery),
             )
         }
 
