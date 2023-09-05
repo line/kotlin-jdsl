30714b6db6f701e32ab313bd8a1cee7ecc545662
jonghyon.s <jonghyon.s@linecorp.com>
2023-09-05 09:03:46 +0900

    feat: predicates in a select clause

--- query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/predicate/Predicate.kt
+++ query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/predicate/Predicate.kt
@@ -2,8 +2,9 @@ package com.linecorp.kotlinjdsl.querymodel.jpql.predicate
 
 import com.linecorp.kotlinjdsl.SinceJdsl
 import com.linecorp.kotlinjdsl.querymodel.QueryPart
+import com.linecorp.kotlinjdsl.querymodel.jpql.expression.Expression
 
 @SinceJdsl("3.0.0")
-interface Predicate : Predicatable, QueryPart {
+interface Predicate : Predicatable, Expression<Boolean>, QueryPart {
     override fun toPredicate(): Predicate = this
 }
