e23c7fae529770c06c182ef1bdb5061f4889ce6e
SeongYunKim <cauboy17@naver.com>
2023-09-07 14:28:17 +0900

    feat: add eclipse-support unit test

---
+++ support/eclipselink/src/test/kotlin/com/linecorp/kotlinjdsl/support/eclipselink/extension/JpaEntityManagerExtensionsTest.kt
@@ -0,0 +1,159 @@
+package com.linecorp.kotlinjdsl.support.eclipselink.extension
+
+import com.linecorp.kotlinjdsl.querymodel.jpql.delete.DeleteQuery
+import com.linecorp.kotlinjdsl.querymodel.jpql.select.SelectQuery
+import com.linecorp.kotlinjdsl.querymodel.jpql.update.UpdateQuery
+import com.linecorp.kotlinjdsl.render.RenderContext
+import com.linecorp.kotlinjdsl.support.eclipselink.JpqlJpaEntityManagerUtils
+import io.mockk.junit5.MockKExtension
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkObject
+import io.mockk.verifySequence
+import jakarta.persistence.Query
+import jakarta.persistence.TypedQuery
+import org.assertj.core.api.WithAssertions
+import org.eclipse.persistence.jpa.JpaEntityManager
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class JpaEntityManagerExtensionsTest : WithAssertions {
+    @MockK
+    private lateinit var entityManager: JpaEntityManager
+
+    @MockK
+    private lateinit var selectQuery: SelectQuery<String>
+
+    @MockK
+    private lateinit var updateQuery: UpdateQuery<String>
+
+    @MockK
+    private lateinit var deleteQuery: DeleteQuery<String>
+
+    @MockK
+    private lateinit var queryParams: Map<String, Any?>
+
+    @MockK
+    private lateinit var query: Query
+
+    @MockK
+    private lateinit var typedQuery: TypedQuery<String>
+
+    @MockK
+    private lateinit var context: RenderContext
+
+    @BeforeEach
+    fun setUp() {
+        mockkObject(JpqlJpaEntityManagerUtils)
+    }
+
+    @Test
+    fun `createQuery - select query`() {
+        // given
+        every {
+            JpqlJpaEntityManagerUtils.createQuery(any(), any<SelectQuery<String>>(), any())
+        } returns typedQuery
+
+        // when
+        val actual = entityManager.createQuery(selectQuery, context)
+
+        // then
+        assertThat(actual).isEqualTo(typedQuery)
+
+        verifySequence {
+            JpqlJpaEntityManagerUtils.createQuery(entityManager, selectQuery, context)
+        }
+    }
+
+    @Test
+    fun `createQuery - select query with query params`() {
+        // given
+        every {
+            JpqlJpaEntityManagerUtils.createQuery(any(), any<SelectQuery<String>>(), any(), any())
+        } returns typedQuery
+
+        // when
+        val actual = entityManager.createQuery(selectQuery, queryParams, context)
+
+        // then
+        assertThat(actual).isEqualTo(typedQuery)
+
+        verifySequence {
+            JpqlJpaEntityManagerUtils.createQuery(entityManager, selectQuery, queryParams, context)
+        }
+    }
+
+    @Test
+    fun `createQuery - update query`() {
+        // given
+        every {
+            JpqlJpaEntityManagerUtils.createQuery(any(), any<UpdateQuery<String>>(), any())
+        } returns query
+
+        // when
+        val actual = entityManager.createQuery(updateQuery, context)
+
+        // then
+        assertThat(actual).isEqualTo(query)
+
+        verifySequence {
+            JpqlJpaEntityManagerUtils.createQuery(entityManager, updateQuery, context)
+        }
+    }
+
+    @Test
+    fun `createQuery - update query with query params`() {
+        // given
+        every {
+            JpqlJpaEntityManagerUtils.createQuery(any(), any<UpdateQuery<String>>(), any(), any())
+        } returns query
+
+        // when
+        val actual = entityManager.createQuery(updateQuery, queryParams, context)
+
+        // then
+        assertThat(actual).isEqualTo(query)
+
+        verifySequence {
+            JpqlJpaEntityManagerUtils.createQuery(entityManager, updateQuery, queryParams, context)
+        }
+    }
+
+    @Test
+    fun `createQuery - delete query`() {
+        // given
+        every {
+            JpqlJpaEntityManagerUtils.createQuery(any(), any<DeleteQuery<String>>(), any())
+        } returns query
+
+        // when
+        val actual = entityManager.createQuery(deleteQuery, context)
+
+        // then
+        assertThat(actual).isEqualTo(query)
+
+        verifySequence {
+            JpqlJpaEntityManagerUtils.createQuery(entityManager, deleteQuery, context)
+        }
+    }
+
+    @Test
+    fun `createQuery - delete query with query params`() {
+        // given
+        every {
+            JpqlJpaEntityManagerUtils.createQuery(any(), any<DeleteQuery<String>>(), any(), any())
+        } returns query
+
+        // when
+        val actual = entityManager.createQuery(deleteQuery, queryParams, context)
+
+        // then
+        assertThat(actual).isEqualTo(query)
+
+        verifySequence {
+            JpqlJpaEntityManagerUtils.createQuery(entityManager, deleteQuery, queryParams, context)
+        }
+    }
+}
