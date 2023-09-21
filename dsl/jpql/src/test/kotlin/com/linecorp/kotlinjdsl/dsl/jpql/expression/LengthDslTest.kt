dfe94826fde1aebf22bf229c734ed80b2cb8c85a
waahhh <waahhh88@gmail.com>
2023-09-21 22:10:21 +0900

    test(dsl): for each length method overloaded

--- dsl/jpql/src/test/kotlin/com/linecorp/kotlinjdsl/dsl/jpql/expression/LengthDslTest.kt
+++ dsl/jpql/src/test/kotlin/com/linecorp/kotlinjdsl/dsl/jpql/expression/LengthDslTest.kt
@@ -7,7 +7,26 @@ import org.assertj.core.api.WithAssertions
 import org.junit.jupiter.api.Test
 
 class LengthDslTest : WithAssertions {
-    private val stringExpression1: Expression<String> = Expressions.value("string1")
+    private val string1 = "string1"
+
+    private val stringExpression1 = Expressions.value("string1")
+
+    @Test
+    fun `length() with a string`() {
+        // when
+        val expression = queryPart {
+            length(string1)
+        }.toExpression()
+
+        val actual: Expression<Int> = expression // for type check
+
+        // then
+        val expected = Expressions.length(
+            Expressions.value(string1),
+        )
+
+        assertThat(actual).isEqualTo(expected)
+    }
 
     @Test
     fun `length() with a string expression`() {
