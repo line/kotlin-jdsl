067e87238648802f52f0d93a8cade3bfd37dedc0
waahhh <waahhh88@gmail.com>
2023-09-21 21:25:35 +0900

    test(dsl): for each locate method overloaded

--- dsl/jpql/src/test/kotlin/com/linecorp/kotlinjdsl/dsl/jpql/expression/LocateDslTest.kt
+++ dsl/jpql/src/test/kotlin/com/linecorp/kotlinjdsl/dsl/jpql/expression/LocateDslTest.kt
@@ -9,9 +9,87 @@ import org.assertj.core.api.WithAssertions
 import org.junit.jupiter.api.Test
 
 class LocateDslTest : WithAssertions {
-    private val stringExpression1 = Expressions.value("ok")
+    private val string1 = "string1"
+    private val string2 = "string2"
+    private val int1 = 1
+
+    private val stringExpression1 = Expressions.value("string1")
     private val stringExpression2 = path(Book::title)
     private val intExpression1 = Expressions.value(1)
+
+    @Test
+    fun `locate() with strings`() {
+        // when
+        val expression = queryPart {
+            locate(string1, string2)
+        }.toExpression()
+
+        val actual: Expression<Int> = expression // for type check
+
+        // then
+        val expected = Expressions.locate(
+            substring = Expressions.value(string1),
+            string = Expressions.value(string2),
+        )
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `locate() with strings and an int`() {
+        // when
+        val expression = queryPart {
+            locate(string1, string2, int1)
+        }.toExpression()
+
+        val actual: Expression<Int> = expression // for type check
+
+        // then
+        val expected = Expressions.locate(
+            substring = Expressions.value(string1),
+            string = Expressions.value(string2),
+            start = Expressions.value(int1),
+        )
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `locate() with a string and a string expression`() {
+        // when
+        val expression = queryPart {
+            locate(string1, stringExpression1)
+        }.toExpression()
+
+        val actual: Expression<Int> = expression // for type check
+
+        // then
+        val expected = Expressions.locate(
+            substring = Expressions.value(string1),
+            string = stringExpression1,
+        )
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `locate() with a string and a string expression and an int`() {
+        // when
+        val expression = queryPart {
+            locate(string1, stringExpression1, int1)
+        }.toExpression()
+
+        val actual: Expression<Int> = expression // for type check
+
+        // then
+        val expected = Expressions.locate(
+            substring = Expressions.value(string1),
+            string = stringExpression1,
+            start = Expressions.value(int1),
+        )
+
+        assertThat(actual).isEqualTo(expected)
+    }
 
     @Test
     fun `locate() with string expressions`() {
@@ -32,7 +110,7 @@ class LocateDslTest : WithAssertions {
     }
 
     @Test
-    fun `locate() with string expressions and a int expression`() {
+    fun `locate() with string expressions and an int expression`() {
         // when
         val expression = queryPart {
             locate(stringExpression1, stringExpression2, intExpression1)
