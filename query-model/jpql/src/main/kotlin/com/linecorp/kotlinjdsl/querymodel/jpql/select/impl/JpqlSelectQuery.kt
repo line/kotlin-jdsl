61e6468693d24100113f4f1cbefb2acf5989ef1d
jonghyon.s <jonghyon.s@linecorp.com>
2023-10-09 08:49:49 +0900

    feat: validation in select query

--- query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/select/impl/JpqlSelectQuery.kt
+++ query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/select/impl/JpqlSelectQuery.kt
@@ -18,4 +18,14 @@ data class JpqlSelectQuery<T : Any> internal constructor(
     val groupBy: Iterable<Expression<*>>?,
     val having: Predicate?,
     val orderBy: Iterable<Sort>?,
-) : SelectQuery<T>
+) : SelectQuery<T> {
+    init {
+        require(select.any()) {
+            "There is no select in a select query. The expression list in the select clause may be emptyList."
+        }
+
+        require(from.any()) {
+            "There is no from in a select query. The from list in the from clause may be emptyList."
+        }
+    }
+}
