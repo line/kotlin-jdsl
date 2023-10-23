63381a87caa5c68899fc552f8c45694bb9ef8e02
waahhh <waahhh88@gmail.com>
2023-10-24 03:06:39 +0900

    feat(query-model): add JpqlUpper

---
+++ query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/expression/impl/JpqlUpper.kt
@@ -0,0 +1,9 @@
+package com.linecorp.kotlinjdsl.querymodel.jpql.expression.impl
+
+import com.linecorp.kotlinjdsl.Internal
+import com.linecorp.kotlinjdsl.querymodel.jpql.expression.Expression
+
+@Internal
+data class JpqlUpper internal constructor(
+    val string: Expression<String>,
+) : Expression<String>
