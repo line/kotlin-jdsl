016749677e83f7290638f92295181aa525c4ab40
Jake Son <sonjeabin@gmail.com>
2023-11-06 21:47:28 +0900

    refactor(query-model): change property type to KCallable

--- query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/path/impl/JpqlEntityProperty.kt
+++ query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/path/impl/JpqlEntityProperty.kt
@@ -3,10 +3,10 @@ package com.linecorp.kotlinjdsl.querymodel.jpql.path.impl
 import com.linecorp.kotlinjdsl.Internal
 import com.linecorp.kotlinjdsl.querymodel.jpql.entity.Entity
 import com.linecorp.kotlinjdsl.querymodel.jpql.path.Path
-import kotlin.reflect.KProperty1
+import kotlin.reflect.KCallable
 
 @Internal
 data class JpqlEntityProperty<T : Any, V> internal constructor(
-    val entity: Entity<*>,
-    val property: KProperty1<in T, V>,
+    val entity: Entity<T>,
+    val property: KCallable<V>,
 ) : Path<V & Any>
