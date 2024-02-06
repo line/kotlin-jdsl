16d71d649cb97b512692231b20ac1037a35660e6
jonghyon.s <jonghyon.s@linecorp.com>
2024-02-06 18:31:30 +0900

    style: format code

--- query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/expression/impl/JpqlCurrentTime.kt
+++ query-model/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/querymodel/jpql/expression/impl/JpqlCurrentTime.kt
@@ -3,5 +3,6 @@ package com.linecorp.kotlinjdsl.querymodel.jpql.expression.impl
 import com.linecorp.kotlinjdsl.Internal
 import com.linecorp.kotlinjdsl.querymodel.jpql.expression.Expression
 import java.sql.Time
+
 @Internal
 object JpqlCurrentTime : Expression<Time>
