8e8b229e8c5f7dae75bd362cdd32a568c486386a
jonghyon.s <jonghyon.s@linecorp.com>
2024-03-04 23:10:56 +0900

    style: revert test code

--- example/spring-data-jpa/src/main/kotlin/com/linecorp/kotlinjdsl/example/spring/data/jpa/jpql/repository/book/BookRepository.kt
+++ example/spring-data-jpa/src/main/kotlin/com/linecorp/kotlinjdsl/example/spring/data/jpa/jpql/repository/book/BookRepository.kt
@@ -1,21 +1,8 @@
 package com.linecorp.kotlinjdsl.example.spring.data.jpa.jpql.repository.book
 
-import com.linecorp.kotlinjdsl.dsl.jpql.Jpql
 import com.linecorp.kotlinjdsl.example.spring.data.jpa.jpql.entity.book.Book
 import com.linecorp.kotlinjdsl.example.spring.data.jpa.jpql.entity.book.Isbn
-import com.linecorp.kotlinjdsl.querymodel.jpql.JpqlQueryable
-import com.linecorp.kotlinjdsl.querymodel.jpql.select.SelectQuery
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
-import jakarta.persistence.LockModeType
-import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
-import org.springframework.data.jpa.repository.QueryHints
 
-interface BookRepository : JpaRepository<Book, Isbn>, KotlinJdslJpqlExecutor {
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @QueryHints(QueryHint(name = "foo", value = "bar"))
-    override fun <T : Any> findAll(
-        init: Jpql.() -> JpqlQueryable<SelectQuery<T>>,
-    ): List<T?>
-}
+interface BookRepository : JpaRepository<Book, Isbn>, KotlinJdslJpqlExecutor
