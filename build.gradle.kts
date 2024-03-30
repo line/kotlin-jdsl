bee2a764b7237517b58b0a74d5a8abd9876f427f
jonghyon.s <jonghyon.s@linecorp.com>
2024-03-30 14:02:25 +0900

    chore: release 3.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ allprojects {
     apply(plugin = "signing")
 
     group = "com.linecorp.kotlin-jdsl"
-    version = "3.4.0-SNAPSHOT"
+    version = "3.4.0"
 
     repositories {
         mavenCentral()
