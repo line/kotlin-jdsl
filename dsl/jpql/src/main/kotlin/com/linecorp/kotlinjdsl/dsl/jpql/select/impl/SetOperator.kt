3f51bc2f49c5023069adf462377024bfd23640e7
Hyunsik Kang <cj848@hanmail.net>
2025-07-03 10:21:33 +0900

    fix: apply reviews

--- dsl/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/dsl/jpql/select/impl/SetOperator.kt
+++ dsl/jpql/src/main/kotlin/com/linecorp/kotlinjdsl/dsl/jpql/select/impl/SetOperator.kt
@@ -1,11 +1,10 @@
 package com.linecorp.kotlinjdsl.dsl.jpql.select.impl
 
-import com.linecorp.kotlinjdsl.Internal
 import com.linecorp.kotlinjdsl.SinceJdsl
 
 @SinceJdsl("3.6.0")
-@Internal
-enum class SetOperator {
+@PublishedApi
+internal enum class SetOperator {
     UNION,
     UNION_ALL,
 }
