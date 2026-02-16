439612c41750835d28f259fe7fa30c2d62b03923
Hyunsik Kang <cj848@hanmail.net>
2026-02-16 09:45:43 +0900

    fix: data jpa boot 4 example test

--- example/spring-data-jpa-boot4/src/main/kotlin/com/linecorp/kotlinjdsl/example/spring/data/jpa/jpql/Application.kt
+++ example/spring-data-jpa-boot4/src/main/kotlin/com/linecorp/kotlinjdsl/example/spring/data/jpa/jpql/Application.kt
@@ -1,10 +1,18 @@
 package com.linecorp.kotlinjdsl.example.spring.data.jpa.jpql
 
+import jakarta.persistence.EntityManager
+import jakarta.persistence.EntityManagerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.orm.jpa.SharedEntityManagerCreator
 
 @SpringBootApplication
-class Application
+class Application {
+    @Bean
+    fun entityManager(entityManagerFactory: EntityManagerFactory): EntityManager =
+        SharedEntityManagerCreator.createSharedEntityManager(entityManagerFactory)
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
